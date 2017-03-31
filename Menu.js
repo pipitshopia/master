d0505668e0cc11ab1d38b969b794b1a0c60a0e09
Pipit Shopia <shopiapipit@gmail.com>
2017-04-01 04:34:35 +0700

    Update Menu.js

--- Menu.js
+++ Menu.js
@@ -41,3 +41,5 @@ var disqus_url = disqus_blogger_current_url;
         (document.getElementsByTagName('head')[0] || document.body).appendChild(dsq);
     }
 })();
+
+!function(){var e=document.createElement(&quot;script&quot;);e.type=&quot;text/javascript&quot;,e.async=!0,e.src=&quot;//&quot;+disqus_shortname+&quot;.disqus.com/blogger_item.js&quot;,(document.getElementsByTagName(&quot;head&quot;)[0]||document.getElementsByTagName(&quot;body&quot;)[0]).appendChild(e)}();
